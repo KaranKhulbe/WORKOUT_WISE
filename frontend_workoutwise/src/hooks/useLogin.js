ef094e3154e7ecf3d89bc24a24e2ac11b2ae4f4f
KaranKhulbe <karankhulbe24@gmail.com>
2024-10-05 10:16:36 +0530

    commit changes

--- frontend_workoutwise/src/hooks/useLogin.js
+++ frontend_workoutwise/src/hooks/useLogin.js
@@ -18,7 +18,7 @@ export const useLogin = () => {
     const json = await response.json()
 
     if (!response.ok) {
-      console.log("error: ", !response.ok)
+      console.log("errorkaran: ", !response.ok)
       setIsLoading(false)
       setError(json.error)
     }
